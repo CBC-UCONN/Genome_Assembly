491aee05282187d6eb25dc7b20818cf8249ebe5f
golden75 <neranjan007@gmail.com>
2021-01-13 21:03:29 -0500

    SOAP

--- short_read_assembly/03_assembly/SOAP/SOAPdenovo.sh
+++ short_read_assembly/03_assembly/SOAP/SOAPdenovo.sh
@@ -31,13 +31,14 @@ SOAPdenovo-127mer all \
         -p 8 \
         -R \
         -o graph_Sample_135 1>ass135.log 2>ass135.err
-
+ 
+ 
 SOAPdenovo-127mer all \
         -s config_file \
         -K 141 \
         -p 8\
         -R \
-        -o graph_Sample_141 1>ass141.log 2>ass141.err
+        -o graph_Sample_141 1>ass141.log 2>ass141.err 
 
 module unload SOAP-denovo/2.04
 
