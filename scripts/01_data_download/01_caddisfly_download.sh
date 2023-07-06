7ebe9649852a498ca77cba5e132c6d8f7e52a7e8
mianahom <mia.nahom@uconn.edu>
2023-07-06 14:07:44 -0400

    data download

---
+++ scripts/01_data_download/01_caddisfly_download.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --job-name=caddisfly_download
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 6
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=END
+#SBATCH --mail-user=your.email@uconn.edu
+#SBATCH --mem=40G
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+# run this script to download the data from the SRA
+
+
+# PacBio HiFi genome of Atopsyche davidsoni: larvae whole body
+    # bioproject: PRJNA741212
+        # biosample: SAMN20492399 
+        # SRA runs:
+            # SRR15654800
+
+# load software
+module load sratoolkit/3.0.2
+
+# output directory, create if it doesn't exist
+
+
+# pacbio outdir
+
+PACBIO=../pacbio
+mkdir -p ${PACBIO}
+
+cd ${PACBIO}
+
+fastq-dump --gzip SRR15654800
+
+
+date
